7a7408b4cae221381482a0482829e8a68362595b
T0R0NT0T0KY0 <Robbin-the-Bobbin@yandex.ru>
2025-02-07 11:01:58 +0300

    refactor: sql

--- src/upsert/single-row.ts
+++ src/upsert/single-row.ts
@@ -5,8 +5,7 @@ export const upsertSingleRow = async (data: StatsData[], tableName: string) => {
 	await Promise.all(
 		data.map((row) => dbClient.query(
 			`INSERT INTO ${tableName} ("organizationId", "cardId", "date", "barcode", "ordersCount", "ordersAmount",
-                             "allOrdersCount", "allOrdersAmount", "saleCount", "saleAmount", "logistic", "commission",
-                             "remains")
+                             "allOrdersCount", "allOrdersAmount", "saleCount", "saleAmount", "logistic", "commission", "remains")
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
           ON CONFLICT ("cardId", "date", "barcode")
               DO UPDATE
